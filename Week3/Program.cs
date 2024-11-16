8b92df0d3d0cdf0c595a2e71ab81adc9469da2f0
Jonathan Bartolo <jonathan.e.bartolo@gmail.com>
2024-11-15 20:32:31 -0800

    Initial Commit

---
+++ Week3/Program.cs
@@ -0,0 +1,12 @@
+﻿double number = 16;
+double guess = number / 2;
+double nextGuess = (guess + number / guess) / 2;
+    while (Math.Abs(nextGuess - guess) > 1e-4) {
+        Console.Write("{0:R}", nextGuess);
+        guess = nextGuess;
+        nextGuess = (guess + number / guess) / 2;
+        Console.ReadLine();
+    }
+Console.WriteLine($"The aproximate square root of {number} is {nextGuess}");
+
+
